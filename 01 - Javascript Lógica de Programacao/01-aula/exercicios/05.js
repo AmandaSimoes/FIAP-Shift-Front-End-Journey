87beebc9e4a3e8fbee0a536f287c954fb3593a98
Amanda Simões <amandasimoesdev@gmail.com>
2023-03-27 19:40:40 -0300

    shift

---
+++ 01 - Javascript Lógica de Programacao/01-aula/exercicios/05.js
@@ -0,0 +1,11 @@
+/*
+
+Montar um script onde o usuário entre com o valor em Reais e faça a sua respectiva conversão para:
+Dólar Americano – Euro - Peso Argentino - Libra Esterlina - Iene
+Exibir na tela todos os valores obtidos com apenas 02 casas decimais.
+Valor em dólar:
+Valor em euro:
+...
+
+
+*/
---
+++ 01 - Javascript Lógica de Programacao/02-aula/codigo-aula/valoresRelacionais.js
@@ -0,0 +1,18 @@
+console.log(5 > 6);
+
+console.log("--------------")
+
+console.log(5 < 6);
+
+console.log("--------------")
+
+let idade = 18;
+console.log(idade >= 18);
+
+console.log("--------------")
+
+let nome = "Amanda";
+console.log(nome == "Fernanda");
+
+console.log("--------------")
+
