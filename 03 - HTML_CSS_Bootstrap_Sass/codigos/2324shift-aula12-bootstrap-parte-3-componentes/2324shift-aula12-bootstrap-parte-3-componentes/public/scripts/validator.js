783c11931dce95e5043eef3a84d2121919394bf6
Amanda Simões <amandasimoesdev@gmail.com>
2023-06-12 18:40:04 -0300

    Add files via upload
    aula 09 a 12

---
+++ 03 - HTML_CSS_Bootstrap_Sass/codigos/2324shift-aula12-bootstrap-parte-3-componentes/2324shift-aula12-bootstrap-parte-3-componentes/public/scripts/validator.js
@@ -0,0 +1,27 @@
+(() => {
+  'use strict';
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll('.needs-validation');
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      'submit',
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+          form.classList.add('was-validated');
+
+          return;
+        }
+
+        form.classList.add('was-validated');
+
+        console.log('O formulário está correto e pode ser enviado');
+      },
+      false
+    );
+  });
+})();
